1854ae7813306a83b4d9a8edd4026f2e1afbfb11
Dygwah98 <36772959+Dygwah98@users.noreply.github.com>
2020-02-25 14:01:28 +0100

    Update header.js

--- src/main/webapp/assets/js/header.js
+++ src/main/webapp/assets/js/header.js
@@ -7,22 +7,6 @@ function usernameButton() {
     }
 }
 
-window.loadGAPI = function() {
-    //you can safely call gapi here
-};
-(function(d, s, id) {
-    var js,
-        p = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src =
-        'https://apis.google.com/js/platform.js?onload=gapiLoaded';
-    p.parentNode.insertBefore(js, p);
-})(document, 'script', 'google-api-js');
-
 $(document).ready(function(){
     usernameButton();
 });
