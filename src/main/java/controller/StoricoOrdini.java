d78bb46e9eff9fea0b6b97ae4608e13ca2d2a4b0
Dygwah98 <36772959+Dygwah98@users.noreply.github.com>
2020-02-26 19:33:52 +0100

    not working

---
+++ src/main/java/controller/StoricoOrdini.java
@@ -0,0 +1,47 @@
+package controller;
+
+import model.Camera;
+import model.Ordine;
+import persistence.DAOFactory;
+import persistence.DBManager;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+@WebServlet(value = "/storico", name = "storico")
+public class StoricoOrdini extends HttpServlet {
+
+    private static final long serialVersionUID = 829948915089756101L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+        try {
+
+            DAOFactory factory = DBManager.getInstance().getDAOFactory();
+
+            Integer idCliente = (Integer)req.getSession().getAttribute("idcliente");
+            List<Ordine> ordine = factory.getOrdineDao().retrieveForStorico(idCliente);
+
+            ConcurrentHashMap<Integer, List<Camera>> camere = new ConcurrentHashMap<>();
+            for(Ordine o : ordine) {
+                List<Camera> c = factory.getCameraDao().retrieveByOrder(o.getIdOrdine());
+                camere.put(o.getIdOrdine(), c);
+            }
+
+            req.getSession().setAttribute("ordine", ordine);
+            req.getSession().setAttribute("camere", camere);
+
+            resp.setStatus(201);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500);
+        }
+    }
+}
--- src/main/java/persistence/jdbc/OrdineDaoJDBC.java
+++ src/main/java/persistence/jdbc/OrdineDaoJDBC.java
@@ -148,7 +148,7 @@ public class OrdineDaoJDBC implements OrdineDao {
     @Override
     public Integer retrieveByUser(Integer Cliente) {
         Integer ret = null;
-        try (JDBCQueryHandler handler = new JDBCQueryHandler("SELECT id_ordine from ordine where fk_cliente=?")) {
+        try (JDBCQueryHandler handler = new JDBCQueryHandler("SELECT id_ordine from ordine where fk_cliente=? and fk_pagamento IS NULL")) {
 
             handler.getStatement().setInt(1, Cliente);
             handler.execute();
@@ -156,6 +156,29 @@ public class OrdineDaoJDBC implements OrdineDao {
             if(handler.existsResultSet()) {
                 handler.getResultSet().next();
                 ret = handler.getResultSet().getInt("id_ordine");
+            }
+
+            return ret;
+
+        } catch (SQLException e) {
+            throw new PersistenceException(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Ordine> retrieveForStorico(Integer Cliente) {
+        List<Ordine> ret = null;
+        try (JDBCQueryHandler handler = new JDBCQueryHandler("SELECT * from ordine where fk_cliente=?")) {
+
+            handler.getStatement().setInt(1, Cliente);
+            handler.execute();
+
+            if(handler.existsResultSet()) {
+                ret = new ArrayList<>();
+                ResultSet res = handler.getResultSet();
+                while(res.next()) {
+                    ret.add(extractTo(res));
+                }
             }
 
             return ret;
